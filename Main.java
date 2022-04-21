36929789935a456aa8660479166c55f03c82f373
Soviena <rovino.rs@gmail.com>
2022-04-21 20:16:08 +0800

    refactor to .live

--- Main.java
+++ Main.java
@@ -8,38 +8,38 @@ public class Main {
         System.out.println("Sinopsis :\n");
         System.out.println(anime.getDesc());
         System.out.print("\nPilih episode [ 1 - "+String.valueOf(anime.getEpList().length)+" ] : "); y = Integer.valueOf(input.nextLine());
-        String link = web.selectMirror(anime.getEpList()[anime.getEpList().length-y]);
-        link = Scrapper.uservideo(link);
-        try {
-            Process prc = Runtime.getRuntime().exec("mpv "+link);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        // String link = web.selectMirror(anime.getEpList()[anime.getEpList().length-y]);
+        // link = Scrapper.uservideo(link);
+        // try {
+        //     Process prc = Runtime.getRuntime().exec("mpv "+link);
+        // } catch (IOException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
     }
     public static void main(String[] args) {
-        Website anoboy = new Website("https://anoboy.online");
-        anoboy.setSearchUrl("/search/");
+        // Website anoboy = new Website("https://anoboy.online");
+        // anoboy.setSearchUrl("/search/");
         String x = "0";
         Anime anime;
-        while (x != "x") {
-            x = Menu.mainMenu();
-            switch (x) {
-                case "s":
-                    anime = Menu.searchAnime(anoboy);                    
-                    selectEpisode(anime, anoboy);
-                    break;
-                case "a":
-                    anime = Menu.recentUpload(anoboy);
-                    selectEpisode(anime, anoboy);
-                    break;
-                case "x":
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Tidak Valid");
-                    break;
-            }
-        }
+        // while (x != "x") {
+        //     x = Menu.mainMenu();
+        //     switch (x) {
+        //         case "s":
+        //             anime = Menu.searchAnime(anoboy);                    
+        //             selectEpisode(anime, anoboy);
+        //             break;
+        //         case "a":
+        //             anime = Menu.recentUpload(anoboy);
+        //             selectEpisode(anime, anoboy);
+        //             break;
+        //         case "x":
+        //             System.exit(0);
+        //             break;
+        //         default:
+        //             System.out.println("Tidak Valid");
+        //             break;
+        //     }
+        // }
     }
 }
--- Menu.java
+++ Menu.java
@@ -12,33 +12,33 @@ public class Menu {
         return x.toLowerCase();
     }
 
-    public static Anime searchAnime(Website web) {
-        Scanner input= new Scanner(System.in); 
-        System.out.print("Cari Anime : "); String query = input.nextLine();
-        Anime[] animes = web.searchAnime(query);
-        int i = 1;
-        for (Anime anime : animes) {
-            System.out.printf("[%d] %s\n",i,anime.getTitle().replaceAll("Nonton anime ", "").replaceAll("Sub Indo", ""));
-            i++;
-        }
-        System.out.print("\nPilih Anime : "); i = Integer.valueOf(input.nextLine());
-        Anime anime = web.getAnimeDetails(animes[i-1]);
-        anime = web.getEpList(anime);
-        return anime;
-    }
+    // public static Anime searchAnime(Website web) {
+    //     Scanner input= new Scanner(System.in); 
+    //     System.out.print("Cari Anime : "); String query = input.nextLine();
+    //     Anime[] animes = web.searchAnime(query);
+    //     int i = 1;
+    //     for (Anime anime : animes) {
+    //         System.out.printf("[%d] %s\n",i,anime.getTitle().replaceAll("Nonton anime ", "").replaceAll("Sub Indo", ""));
+    //         i++;
+    //     }
+    //     System.out.print("\nPilih Anime : "); i = Integer.valueOf(input.nextLine());
+    //     Anime anime = web.getAnimeDetails(animes[i-1]);
+    //     anime = web.getEpList(anime);
+    //     return anime;
+    // }
 
-    public static Anime recentUpload(Website web){
-        Scanner input= new Scanner(System.in); 
-        Anime[] animes = web.searchAnime("");
-        int i = 1;
-        for (Anime anime : animes) {
-            System.out.printf("[%d] %s\n",i,anime.getTitle().replaceAll("Nonton anime ", "").replaceAll("Sub Indo", ""));
-            i++;
-        }
-        System.out.print("\nPilih Anime : "); i = Integer.valueOf(input.nextLine());
-        Anime anime = web.getAnimeDetails(animes[i-1]);
-        anime = web.getEpList(anime);
-        return anime;        
-    }
+    // public static Anime recentUpload(Website web){
+    //     Scanner input= new Scanner(System.in); 
+    //     Anime[] animes = web.searchAnime("");
+    //     int i = 1;
+    //     for (Anime anime : animes) {
+    //         System.out.printf("[%d] %s\n",i,anime.getTitle().replaceAll("Nonton anime ", "").replaceAll("Sub Indo", ""));
+    //         i++;
+    //     }
+    //     System.out.print("\nPilih Anime : "); i = Integer.valueOf(input.nextLine());
+    //     Anime anime = web.getAnimeDetails(animes[i-1]);
+    //     anime = web.getEpList(anime);
+    //     return anime;        
+    // }
 
 }
