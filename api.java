1c50d9b42e0647855ef0f709a6fe2d0c0df119ca
Soviena <rovino.rs@gmail.com>
2022-04-23 21:29:02 +0800

    query progress

--- api.java
+++ api.java
@@ -6,7 +6,18 @@ public interface api {
     // Keyword = load_movie_last_update
     // Keyword = load_movie_trending
     // Keyword = load_search_movie
-    public JSONObject query(String keyword, boolean ongoing, String query, int page, int limit);
+    public JSONObject query(String keyword, boolean ongoing, String query, int page, int limit){
+        JSONObject data = new JSONObject();
+        data.put("page", page);
+        data.put("limit", limit);
+        data.put("action", keyword);
+        if keyword.equals("load_search_movie"){
+            data.put("keyword", query)
+        }
+        if ongoing {
+            data.put("status", "Ongoing");
+        }
+    }
     public JSONObject query(String keyword, boolean ongoing, String query, int page);
     public JSONObject query(String keyword, boolean ongoing, String query);
     public JSONObject query(String keyword, boolean ongoing);
